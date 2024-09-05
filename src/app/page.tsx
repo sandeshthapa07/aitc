4dfc4d572c96b45841a40c542d9feef563935cf5
sandesh_thapa <sandesh.thapa@aitc.ai>
2024-09-05 17:05:01 +0545

    fix: changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="w-screen flex justify-center items-center h-screen">
-      <h3>sandesh</h3>
+    <div className='flex h-screen w-screen items-center justify-center p-3'>
+      <h3 className=' text-[3rem] font-black'>sandesh</h3>
     </div>
   );
 }
