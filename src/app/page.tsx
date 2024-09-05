5cb1bde528a581bfa732d81117e1e5b02ac44d51
Sandesh thapa <sandesh.thapa@braintip.ai>
2024-09-05 23:31:55 +0545

    fix: changes made

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className='flex h-screen w-screen items-center justify-center p-3'>
+    <div className='sandesh flex h-screen w-screen items-center justify-center bg-red-300 p-3  text-white'>
       <h3 className=' text-[3rem] font-black'>sandesh</h3>
     </div>
   );
